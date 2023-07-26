8662e9fd10b116efb38a1b13f17da89f0cb5f09d
nickurane <63089472+nickurane@users.noreply.github.com>
2023-07-26 18:36:11 +0530

    question selection logic

--- screens/Question.jsx
+++ screens/Question.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import {View,Text,FlatList,StyleSheet} from "react-native"
 import {question} from "./data"
 import QuestionSlide from './QuestionSlide';
@@ -7,18 +7,22 @@ import QuestionSlide from './QuestionSlide';
 
 
 const Question= () => {
+const [correctAnswer,setCorrectAnswer]=useState(0)
+const [wrongAnswer,setWrongAnswer]=useState(0)
+
 
 
 
 
   const renderItem1 = ({ item }) => {
-    return <QuestionSlide item={item} navigation={navigation} />
+    return <QuestionSlide item={item} setCorrectAnswer={setCorrectAnswer} setWrongAnswer={setWrongAnswer} />
   };
 
   return (
     <View style={{flex:1}}>
     <View>
-    <Text>Questions</Text>
+    <Text>Correct Answered:{correctAnswer}</Text>
+    <Text>Wrong Answered:{wrongAnswer}</Text>
     </View>
     <FlatList
       data={question}
